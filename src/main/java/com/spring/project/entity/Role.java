352caaa3bb908fa83cb7ae79753276e4e20ebcf9
[DanailIvanov] <[danailivanov94@gmail.com]>
2018-07-31 01:29:09 +0300

    create a role entity

---
+++ src/main/java/com/spring/project/entity/Role.java
@@ -0,0 +1,44 @@
+package com.spring.project.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    private  Long id;
+
+    private String name;
+
+    private Set<User> users;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToMany(mappedBy = "roles")
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
