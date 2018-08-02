c93348e4dd5edd02d565d4bccb18b99f6a4083ce
Kristina <k.bonevska@yahoo.com>
2018-08-02 14:21:12 +0300

    add findArticleByTitle method

--- src/main/java/com/spring/project/repository/ArticleRepository.java
+++ src/main/java/com/spring/project/repository/ArticleRepository.java
@@ -4,6 +4,9 @@ package com.spring.project.repository;
 import com.spring.project.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    List<Article> findArticlesByTitle(String pSearchTerm);
 }
